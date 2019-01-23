1316090c45c57fde5426194b6768b20adb25f08b
chengfei <chengfei@iuitech.com>
2019-01-23 09:32:51 +0800

    up

---
+++ sale/src/main/java/com/joey/sale/SaleApplication.java
@@ -0,0 +1,14 @@
+package com.joey.sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleApplication.class, args);
+    }
+
+}
+
